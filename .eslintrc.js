946fcbfd00baf84d41ecf4e5d2ae8e538f630668
Limerio <44976231+Limerio@users.noreply.github.com>
2023-10-17 18:20:49 +0200

    Remove rule for eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,7 +23,6 @@ module.exports = {
             { avoidEscape: true, allowTemplateLiterals: true },
         ],
         semi: ['error', 'never'],
-        'no-console': 'error',
         'no-implicit-globals': 'error',
         'no-warning-comments': ['error', { terms: ['fixme', 'todo'] }],
         'newline-before-return': 'error',
